dc5f508f2ce4541fc1ba181d46e0cd61d3fc58fa
byte6 <46079089+1226608@users.noreply.github.com>
2023-07-12 05:20:33 +0800

    Add files via upload

---
+++ 2.0.py
@@ -0,0 +1,20 @@
+import os
+from pydub import AudioSegment
+import pysrt
+def clean_filename(filename):
+    return "".join([c for c in filename if c.isalpha() or c.isdigit() or c==' ' or c=='_']).rstrip()
+def split_audio_by_srt(srt_file_path, input_file_path, output_dir):
+    audio = AudioSegment.from_wav(input_file_path)
+    subs = pysrt.open(srt_file_path)
+    os.makedirs(output_dir, exist_ok=True)
+    for sub in subs:
+        start_time = (sub.start.hours * 3600 + sub.start.minutes * 60 + sub.start.seconds) * 1000 + sub.start.milliseconds
+        end_time = (sub.end.hours * 3600 + sub.end.minutes * 60 + sub.end.seconds) * 1000 + sub.end.milliseconds
+        segment = audio[start_time:end_time]
+        output_filename = clean_filename(f'{sub.index}_{sub.text}') + '.wav'
+        output_path = os.path.join(output_dir, output_filename)
+        segment.export(output_path, format="wav")
+srt_file_path = "都.srt"
+input_file_path = "都.wav"
+output_dir = "存"
+split_audio_by_srt(srt_file_path, input_file_path, output_dir)
