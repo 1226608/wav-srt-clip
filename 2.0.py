dd3606629ea0a5d4b9a42c340803cc85eb8d4599
byte6 <46079089+1226608@users.noreply.github.com>
2023-07-12 23:50:26 +0800

    Update 2.0.py

--- 2.0.py
+++ 2.0.py
@@ -1,18 +1,27 @@
-import os,re,pysrt
+import os,re,pysrt,time
 from pydub import AudioSegment
+
 def clean_filename(filename):
     return re.sub(r'[^\w _-]+', '', filename).strip()
+
 def split_audio_by_srt(srt_file_path, input_file_path, output_dir):
+    start_time = time.time()
     audio = AudioSegment.from_wav(input_file_path)
     subs = pysrt.open(srt_file_path)
     os.makedirs(output_dir, exist_ok=True)
+
     for sub in subs:
-        start_time = (sub.start.hours * 3600 + sub.start.minutes * 60 + sub.start.seconds) * 1000 + sub.start.milliseconds
-        end_time = (sub.end.hours * 3600 + sub.end.minutes * 60 + sub.end.seconds) * 1000 + sub.end.milliseconds
-        segment = audio[start_time:end_time]
+        start_time_sub = (sub.start.hours * 3600 + sub.start.minutes * 60 + sub.start.seconds) * 1000 + sub.start.milliseconds
+        end_time_sub = (sub.end.hours * 3600 + sub.end.minutes * 60 + sub.end.seconds) * 1000 + sub.end.milliseconds
+        segment = audio[start_time_sub:end_time_sub]
         output_filename = clean_filename(f'{sub.index}_{sub.text}') + '.wav'
         output_path = os.path.join(output_dir, output_filename)
         segment.export(output_path, format="wav")
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"处理完成,耗时: {elapsed_time}秒")
+
 srt_file_path = "都.srt"
 input_file_path = "都.wav"
 output_dir = "存"
