cb16124ff7d1c2c69dd703d492b90721919f82be
byte6 <46079089+1226608@users.noreply.github.com>
2023-07-12 23:35:15 +0800

    Add files via upload

---
+++ 2.0_CN.py
@@ -0,0 +1,23 @@
+import os,re,pysrt
+from pydub import AudioSegment
+
+def 清理文件名(文件名):
+    return re.sub(r'[^\w _-]+', '', 文件名).strip()
+
+def 根据字幕分割音频(字幕文件路径, 输入文件路径, 输出目录):
+    音频 = AudioSegment.from_wav(输入文件路径)
+    字幕 = pysrt.open(字幕文件路径)
+    os.makedirs(输出目录, exist_ok=True)
+    
+    for 每条字幕 in 字幕:
+        开始时间 = (每条字幕.start.hours * 3600 + 每条字幕.start.minutes * 60 + 每条字幕.start.seconds) * 1000 + 每条字幕.start.milliseconds
+        结束时间 = (每条字幕.end.hours * 3600 + 每条字幕.end.minutes * 60 + 每条字幕.end.seconds) * 1000 + 每条字幕.end.milliseconds
+        片段 = 音频[开始时间:结束时间]
+        输出文件名 = 清理文件名(f'{每条字幕.index}_{每条字幕.text}') + '.wav'
+        输出路径 = os.path.join(输出目录, 输出文件名)
+        片段.export(输出路径, format="wav")
+
+字幕文件路径 = "都.srt"
+输入文件路径 = "都.wav"
+输出目录 = "存"
+根据字幕分割音频(字幕文件路径, 输入文件路径, 输出目录)
